959d16773ef86a171930633c5e64c877d9313608
ranjit-dey <ranjitdey05265@gmail.com>
2024-09-20 16:08:25 +0530

    different types of operation

---
+++ 01_Basics/04_operation.js
@@ -0,0 +1,52 @@
+console.log("\n\n\n")
+
+/*================================ Operation ================================*/
+
+let value = 3
+let negValue = -value
+
+
+// console.log(negValue)
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 ** 2)
+// console.log(2 % 2)
+
+let str1 = "Hello "
+let str2 = "Ranjit"
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+
+console.log("1" + 2 + 3) // only for show off or question
+console.log(1 + 2 + "3") // only for show off
+
+console.log(3 + 4 * 5 % 3) // use parenthesis readbilty
+
+console.log(true)
+console.log(+true) // (true+) => throws an error
+console.log(-true)
+console.log(+false)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 // not a better for declaration
+
+let gameCounter = 0
+
+gameCounter++
+console.log(gameCounter)
+
+++gameCounter
+console.log(gameCounter)
+
+console.log(gameCounter++)
+console.log(++gameCounter)
+
+
+console.log("\n\n\n")
